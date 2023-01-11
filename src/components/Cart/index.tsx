ac6c268c020c98c1ccc429fe11b2d4f8d455697c
Guilherme Camargo <guilherme1290@outlook.com.br>
2023-01-11 14:40:27 -0300

    first commit

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,138 @@
+import { useState } from 'react';
+import { FlatList, TouchableOpacity } from 'react-native';
+
+import { CartItem } from '../../types/CartItem';
+import { Product } from '../../types/Product';
+import { api } from '../../utils/api';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { Button } from '../Button';
+import { MinusCircle } from '../Icons/MinusCircle';
+import { PlusCircle } from '../Icons/PlusCircle';
+import { OrderConfirmedModal } from '../OrderConfirmedModal';
+import { Text } from '../Text';
+
+import {
+	Actions,
+	Image,
+	Item,
+	ProductContainer,
+	QuantityContainer,
+	ProductDetails,
+	Summary,
+	TotalContainer
+} from './styles';
+
+interface CartProps {
+	cartItems: CartItem[];
+	onAdd: (product: Product) => void;
+	onDecrement: (product: Product) => void;
+	onConfirmOrder: () => void;
+	selectedTable: string;
+}
+
+export function Cart({ cartItems, onAdd, onDecrement, onConfirmOrder, selectedTable }: CartProps) {
+	const [isLoading, setIsLoading] = useState(false);
+	const [isModalVisible, setIsModalVisible] = useState(false);
+
+	const total = cartItems.reduce((acc, cartItem) => {
+		return acc + cartItem.quantity * cartItem.product.price;
+	}, 0);
+
+	async function handleConfirmOrder() {
+		setIsLoading(true);
+
+		const payload = {
+			table: selectedTable,
+			products: cartItems.map((cartItem) => ({
+				product: cartItem.product._id,
+				quantity: cartItem.quantity,
+			})),
+		};
+
+		await api.post('/orders', payload);
+
+		setIsLoading(false);
+		setIsModalVisible(true);
+	}
+
+	function handleOk() {
+		onConfirmOrder();
+		setIsModalVisible(false);
+	}
+
+	return (
+		<>
+			<OrderConfirmedModal
+				visible={isModalVisible}
+				onOk={handleOk}
+			></OrderConfirmedModal>
+
+			{cartItems.length > 0 && (
+				<FlatList
+					data={cartItems}
+					keyExtractor={cartItem => cartItem.product._id}
+					showsVerticalScrollIndicator={false}
+					style={{ marginBottom: 20, maxHeight: 150 }}
+					renderItem={({ item: cartItem }) => (
+						<Item>
+							<ProductContainer>
+								<Image
+									source={{
+										uri: `http://192.168.1.2:3001/uploads/${cartItem.product.imagePath}`,
+									}}
+								></Image>
+
+								<QuantityContainer>
+									<Text color='#666' size={14}>{cartItem.quantity}x</Text>
+								</QuantityContainer>
+
+								<ProductDetails>
+									<Text size={14} weight='600'>{cartItem.product.name}</Text>
+									<Text size={14} color='#666' style={{ marginTop: 4 }}>
+										{formatCurrency(cartItem.product.price)}
+									</Text>
+								</ProductDetails>
+							</ProductContainer>
+
+							<Actions>
+								<TouchableOpacity
+									style={{ marginRight: 24 }}
+									onPress={() => onAdd(cartItem.product)}
+								>
+									<PlusCircle></PlusCircle>
+								</TouchableOpacity>
+
+								<TouchableOpacity
+									onPress={() => onDecrement(cartItem.product)}
+								>
+									<MinusCircle></MinusCircle>
+								</TouchableOpacity>
+							</Actions>
+						</Item>
+					)}
+				></FlatList>
+			)}
+
+			<Summary>
+				<TotalContainer>
+					{cartItems.length > 0 ? (
+						<>
+							<Text color='#666'>Total</Text>
+							<Text size={20} weight='600'>{formatCurrency(total)}</Text>
+						</>
+					) : (
+						<Text color='#666'>Seu carrinho está vazio</Text>
+					)}
+				</TotalContainer>
+
+				<Button
+					onPress={handleConfirmOrder}
+					disabled={cartItems.length === 0}
+					loading={isLoading}
+				>
+					Confirmar pedido
+				</Button>
+			</Summary>
+		</>
+	);
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,36 @@
+import { TouchableOpacity } from 'react-native';
+import { Text } from '../Text';
+import { Container, Content, OrderHeader, Table } from './styles';
+
+interface HeaderProps {
+	selectedTable: string;
+	onCancelOrder: () => void;
+}
+
+export function Header({ selectedTable, onCancelOrder }: HeaderProps) {
+	return (
+		<Container>
+			{!selectedTable && (
+				<>
+					<Text size={14} opacity={0.9}>Bem vindo(a) ao</Text>
+					<Text size={24} weight='700'>WAITER<Text size={24}>APP</Text></Text>
+				</>
+			)}
+
+			{selectedTable && (
+				<Content>
+					<OrderHeader>
+						<Text weight='600' size={24}>Pedido</Text>
+						<TouchableOpacity onPress={onCancelOrder}>
+							<Text size={14} weight='600' color='#D73035'>Cancelar Pedido</Text>
+						</TouchableOpacity>
+					</OrderHeader>
+
+					<Table>
+						<Text color='#666'>Mesa {selectedTable}</Text>
+					</Table>
+				</Content>
+			)}
+		</Container>
+	);
+}
